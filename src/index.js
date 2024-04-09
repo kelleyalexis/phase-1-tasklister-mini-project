f387deb829115d0f64498c630d2fde77afea6ece
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-04-09 17:31:40 -0400

    Creating a click event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const taskValue = document.getElementById('new-task-description').value;
+    createNewTask(taskValue)
+  
+  }) 
+})
+
+function createNewTask(newTask) {
+    
+    let p = document.createElement('p')
+    p.textContent = newTask
+    console.log(p)
+    document.querySelector('#tasks').appendChild(p)
+
+}
+
+
